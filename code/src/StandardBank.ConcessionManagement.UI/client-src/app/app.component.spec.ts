40f9526d4ca1501ecb10d69e15ef2ca0665ece5f
Heathesh Bhandari <heath.bhandari@gmail.com>
2017-07-21 19:39:23 +0200

    Fixing broken karma tests

--- code/src/StandardBank.ConcessionManagement.UI/client-src/app/app.component.spec.ts
+++ code/src/StandardBank.ConcessionManagement.UI/client-src/app/app.component.spec.ts
@@ -1,24 +1,22 @@
 ﻿import { TestBed, async } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from "./app.component";
 import { PageHeaderComponent } from './page-header/page-header.component';
+import { UserService, MockUserService } from "./user/user.service";
 
 describe("AppComponent", () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent, PageHeaderComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule, HttpModule],
+            declarations: [AppComponent, PageHeaderComponent],
+            providers: [{ provide: UserService, useClass: MockUserService }]
+        }).compileComponents();
+    }));
 
-  it("should create the app", async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    it("should create the app", async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 });
--- code/src/StandardBank.ConcessionManagement.UI/client-src/app/page-header/page-header.component.spec.ts
+++ code/src/StandardBank.ConcessionManagement.UI/client-src/app/page-header/page-header.component.spec.ts
@@ -1,25 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+﻿import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 import { PageHeaderComponent } from './page-header.component';
+import { UserService, MockUserService } from "../user/user.service";
 
 describe('PageHeaderComponent', () => {
-  let component: PageHeaderComponent;
-  let fixture: ComponentFixture<PageHeaderComponent>;
+    let component: PageHeaderComponent;
+    let fixture: ComponentFixture<PageHeaderComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PageHeaderComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpModule],
+            declarations: [PageHeaderComponent],
+            providers: [{ provide: UserService, useClass: MockUserService }]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PageHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PageHeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 });
