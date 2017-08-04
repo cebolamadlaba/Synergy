6276b8d621e1f92bb4ab5f690a7c73de9b12eca6
Sifiso <sfiso.dlaba@gmail.com>
2017-08-04 20:08:59 +0200

    fix build

--- code/src/StandardBank.ConcessionManagement.Interface/Services/INotificationService.cs
+++ code/src/StandardBank.ConcessionManagement.Interface/Services/INotificationService.cs
@@ -1,7 +1,8 @@
 namespace StandardBank.ConcessionManagement.Interface.Services
 {
-    using StandardBank.ConcessionManagement.Model;
-    
+  
+    using StandardBank.ConcessionManagement.Model.Common;
+
     public interface INotificationService
     {
          bool SendEmail(EmailMessage emailMessage);
