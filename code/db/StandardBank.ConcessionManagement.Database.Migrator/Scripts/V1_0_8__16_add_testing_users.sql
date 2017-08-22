fc318b31cad8268acd79a4e02d6da8efbb022310
Heathesh Bhandari <heath.bhandari@gmail.com>
2017-08-22 09:05:02 +0200

    Added script to add testing users

---
+++ code/db/StandardBank.ConcessionManagement.Database.Migrator/Scripts/V1_0_8__16_add_testing_users.sql
@@ -0,0 +1,38 @@
+﻿MERGE [dbo].[tblUser] AS TARGET
+USING (VALUES  
+('A215840', 'Mpho.Nxiwa@standardbank.co.za', 'Mpho', 'Nxiwa', 1), 
+('A150458', 'AnthonyEdgar.Ogle@standardbank.co.za', 'Anthony', 'Ogle', 1), 
+('A209089', 'Thomas.Nowe@standardbank.co.za', 'Thomas', 'Nowe', 1)) 
+AS SOURCE ([ANumber], [EmailAddress], [FirstName], [Surname], [IsActive])
+ON TARGET.[ANumber] = SOURCE.[ANumber]
+WHEN NOT MATCHED THEN 
+INSERT ([ANumber], [EmailAddress], [FirstName], [Surname], [IsActive]) 
+VALUES (SOURCE.[ANumber], SOURCE.[EmailAddress], SOURCE.[FirstName], SOURCE.[Surname], SOURCE.[IsActive]);
+
+DECLARE
+	@A215840 int,
+	@A150458 int,
+	@A209089 int,
+	@Requestor int,
+	@BCM int,
+	@PCM int
+
+SELECT @A215840 = [pkUserId] FROM [dbo].[tblUser] WHERE [ANumber] = 'A215840'
+SELECT @A150458 = [pkUserId] FROM [dbo].[tblUser] WHERE [ANumber] = 'A150458'
+SELECT @A209089 = [pkUserId] FROM [dbo].[tblUser] WHERE [ANumber] = 'A209089'
+
+SELECT @Requestor = [pkRoleId] FROM [dbo].[rtblRole] WHERE [RoleName] = 'Requestor'
+SELECT @BCM = [pkRoleId] FROM [dbo].[rtblRole] WHERE [RoleName] = 'BCM'
+SELECT @PCM = [pkRoleId] FROM [dbo].[rtblRole] WHERE [RoleName] = 'PCM'
+
+MERGE [dbo].[tblUserRole] AS TARGET
+USING (VALUES  
+(@A215840, @Requestor, 1),
+(@A150458, @BCM, 1),
+(@A209089, @PCM, 1))
+AS SOURCE ([UserId], [RoleId], [IsActive])
+ON TARGET.[fkUserId] = SOURCE.[UserId]
+WHEN MATCHED THEN
+UPDATE SET [fkRoleId] = SOURCE.[RoleId], [IsActive] = SOURCE.[IsActive]
+WHEN NOT MATCHED THEN
+INSERT ([fkUserId], [fkRoleId], [IsActive]) VALUES (SOURCE.[UserId], SOURCE.[RoleId], SOURCE.[IsActive]);
