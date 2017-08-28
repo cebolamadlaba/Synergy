061a3f73bd0c2d8291134199f9cb3e6c7a65f92f
Heathesh Bhandari <heath.bhandari@gmail.com>
2017-08-28 15:16:29 +0200

    Added a test for transaction manager

--- code/test/StandardBank.ConcessionManagement.BusinessLogic.Test/UnitTest/TransactionalManagerTest.cs
+++ code/test/StandardBank.ConcessionManagement.BusinessLogic.Test/UnitTest/TransactionalManagerTest.cs
@@ -1,4 +1,6 @@
-﻿using StandardBank.ConcessionManagement.Interface.BusinessLogic;
+﻿using Moq;
+using StandardBank.ConcessionManagement.Interface.BusinessLogic;
+using StandardBank.ConcessionManagement.Model.Repository;
 using StandardBank.ConcessionManagement.Test.Helpers;
 using static StandardBank.ConcessionManagement.Test.Helpers.MockedDependencies;
 using Xunit;
@@ -28,10 +30,28 @@ namespace StandardBank.ConcessionManagement.BusinessLogic.Test.UnitTest
         /// <summary>
         /// Tests that GetCashConcessionsForRiskGroupNumber executes positive.
         /// </summary>
-        //[Fact] //TODO: Create and make this test work
+        [Fact] 
         public void GetCashConcessionsForRiskGroupNumber_Executes_Positive()
         {
-            
+            MockPricingManager.Setup(_ => _.GetRiskGroupForRiskGroupNumber(It.IsAny<int>()))
+                .Returns(new Model.UserInterface.Pricing.RiskGroup { Id = 1, Name = "Test Risk Group", Number = 1000 });
+
+            MockConcessionManager.Setup(_ => _.GetConcessionsForRiskGroup(It.IsAny<int>(), It.IsAny<string>()))
+                .Returns(new[] { new Model.UserInterface.Concession() });
+
+            MockConcessionTransactionalRepository.Setup(_ => _.ReadByConcessionId(It.IsAny<int>()))
+                .Returns(new[] { new ConcessionTransactional() });
+
+            MockLegalEntityRepository.Setup(_ => _.ReadById(It.IsAny<int>()))
+                .Returns(new LegalEntity { IsActive = true });
+
+            MockLegalEntityAccountRepository.Setup(_ => _.ReadById(It.IsAny<int>()))
+                .Returns(new LegalEntityAccount { IsActive = true });
+
+            var result = _transactionalManager.GetTransactionalConcessionsForRiskGroupNumber(1);
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
         }
     }
 }
