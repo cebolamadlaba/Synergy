3e4c2c6d851f1129026e2cca6ecd9c483a72a501
Heathesh Bhandari <heath.bhandari@gmail.com>
2017-08-29 10:30:36 +0200

    Working on New Transactional View

--- code/src/StandardBank.ConcessionManagement.UI/client-src/app/transactional-add-concession/transactional-add-concession.component.ts
+++ code/src/StandardBank.ConcessionManagement.UI/client-src/app/transactional-add-concession/transactional-add-concession.component.ts
@@ -7,6 +7,12 @@ import { FormGroup, FormArray, FormBuilder, Validators, FormControl } from '@ang
 import { Location } from '@angular/common';
 import { LookupDataService } from "../services/lookup-data.service";
 import { TransactionalConcessionService } from "../services/transactional-concession.service";
+import { Period } from "../models/period";
+import { PeriodType } from "../models/period-type";
+import { ConditionType } from "../models/condition-type";
+import { ConditionProduct } from "../models/condition-product";
+import { ClientAccount } from "../models/client-account";
+
 
 @Component({
     selector: 'app-transactional-add-concession',
@@ -22,7 +28,20 @@ export class TransactionalAddConcessionComponent implements OnInit, OnDestroy {
     observableRiskGroup: Observable<RiskGroup>;
     riskGroup: RiskGroup;
     riskGroupNumber: number;
+    selectedConditionTypes: ConditionType[];
     isLoading = false;
+
+    observablePeriods: Observable<Period[]>;
+    periods: Period[];
+
+    observablePeriodTypes: Observable<PeriodType[]>;
+    periodTypes: PeriodType[];
+
+    observableConditionTypes: Observable<ConditionType[]>;
+    conditionTypes: ConditionType[];
+
+    observableClientAccounts: Observable<ClientAccount[]>;
+    clientAccounts: ClientAccount[];
 
     constructor(private route: ActivatedRoute,
         private formBuilder: FormBuilder,
@@ -30,6 +49,11 @@ export class TransactionalAddConcessionComponent implements OnInit, OnDestroy {
         @Inject(LookupDataService) private lookupDataService,
         @Inject(TransactionalConcessionService) private transactionalConcessionService) {
         this.riskGroup = new RiskGroup();
+        this.periods = [new Period()];
+        this.periodTypes = [new PeriodType()];
+        this.conditionTypes = [new ConditionType()];
+        this.selectedConditionTypes = [new ConditionType()];
+        this.clientAccounts = [new ClientAccount()];
     }
 
     ngOnInit() {
@@ -39,16 +63,85 @@ export class TransactionalAddConcessionComponent implements OnInit, OnDestroy {
             if (this.riskGroupNumber) {
                 this.observableRiskGroup = this.lookupDataService.getRiskGroup(this.riskGroupNumber);
                 this.observableRiskGroup.subscribe(riskGroup => this.riskGroup = riskGroup, error => this.errorMessage = <any>error);
+
+                this.observableClientAccounts = this.lookupDataService.getClientAccounts(this.riskGroupNumber);
+                this.observableClientAccounts.subscribe(clientAccounts => this.clientAccounts = clientAccounts, error => this.errorMessage = <any>error);
             }
         });
 
         this.transactionalConcessionForm = this.formBuilder.group({
-            //concessionItemRows: this.formBuilder.array([this.initConcessionItemRows()]),
-            //conditionItemsRows: this.formBuilder.array([]),
+            concessionItemRows: this.formBuilder.array([this.initConcessionItemRows()]),
+            conditionItemsRows: this.formBuilder.array([]),
             mrsCrs: new FormControl(),
             smtDealNumber: new FormControl(),
             motivation: new FormControl()
         });
+
+        this.observablePeriods = this.lookupDataService.getPeriods();
+        this.observablePeriods.subscribe(periods => this.periods = periods, error => this.errorMessage = <any>error);
+
+        this.observablePeriodTypes = this.lookupDataService.getPeriodTypes();
+        this.observablePeriodTypes.subscribe(periodTypes => this.periodTypes = periodTypes, error => this.errorMessage = <any>error);
+
+        this.observableConditionTypes = this.lookupDataService.getConditionTypes();
+        this.observableConditionTypes.subscribe(conditionTypes => this.conditionTypes = conditionTypes, error => this.errorMessage = <any>error);
+    }
+
+    initConcessionItemRows() {
+        return this.formBuilder.group({
+            transactionType: [''],
+            accountNumber: [''],
+            tableNumber: [''],
+            flatFeeOrRate: [''],
+            adValorem: ['']
+        });
+    }
+
+    initConditionItemRows() {
+        this.selectedConditionTypes.push(new ConditionType());
+
+        return this.formBuilder.group({
+            conditionType: [''],
+            conditionProduct: [''],
+            interestRate: [''],
+            volume: [''],
+            value: [''],
+            periodType: [''],
+            period: ['']
+        });
+    }
+
+    addNewConcessionRow() {
+        const control = <FormArray>this.transactionalConcessionForm.controls['concessionItemRows'];
+        control.push(this.initConcessionItemRows());
+    }
+
+    addNewConditionRow() {
+        const control = <FormArray>this.transactionalConcessionForm.controls['conditionItemsRows'];
+        control.push(this.initConditionItemRows());
+    }
+
+    addNewConditionRowIfNone() {
+        const control = <FormArray>this.transactionalConcessionForm.controls['conditionItemsRows'];
+        if (control.length == 0)
+            control.push(this.initConditionItemRows());
+    }
+
+    deleteConcessionRow(index: number) {
+        const control = <FormArray>this.transactionalConcessionForm.controls['concessionItemRows'];
+        control.removeAt(index);
+    }
+
+    deleteConditionRow(index: number) {
+        const control = <FormArray>this.transactionalConcessionForm.controls['conditionItemsRows'];
+        control.removeAt(index);
+
+        this.selectedConditionTypes.splice(index, 1);
+    }
+
+    conditionTypeChanged(rowIndex) {
+        const control = <FormArray>this.transactionalConcessionForm.controls['conditionItemsRows'];
+        this.selectedConditionTypes[rowIndex] = control.controls[rowIndex].get('conditionType').value;
     }
 
     goBack() {
