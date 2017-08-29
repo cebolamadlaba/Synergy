0f6fa5e2197e9d7b838ba69896b437c513a1b2b1
Heathesh Bhandari <heath.bhandari@gmail.com>
2017-08-29 13:03:06 +0200

    Bug fix for bol add tests

--- code/src/StandardBank.ConcessionManagement.UI/client-src/app/bol-add-concession/bol-add-concession.component.spec.ts
+++ code/src/StandardBank.ConcessionManagement.UI/client-src/app/bol-add-concession/bol-add-concession.component.spec.ts
@@ -1,25 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BolAddConcessionComponent } from './bol-add-concession.component';
+import { HttpModule } from '@angular/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormArray, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { LookupDataService, MockLookupDataService } from "../services/lookup-data.service";
 
 describe('BolAddConcessionComponent', () => {
-  let component: BolAddConcessionComponent;
-  let fixture: ComponentFixture<BolAddConcessionComponent>;
+    let component: BolAddConcessionComponent;
+    let fixture: ComponentFixture<BolAddConcessionComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BolAddConcessionComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpModule, ModalModule.forRoot(), RouterTestingModule, FormsModule, ReactiveFormsModule],
+            declarations: [BolAddConcessionComponent],
+            providers: [
+                { provide: LookupDataService, useClass: MockLookupDataService }
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BolAddConcessionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BolAddConcessionComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- code/src/StandardBank.ConcessionManagement.UI/client-src/app/bol-add-concession/bol-add-concession.component.ts
+++ code/src/StandardBank.ConcessionManagement.UI/client-src/app/bol-add-concession/bol-add-concession.component.ts
@@ -1,20 +1,76 @@
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { Observable } from "rxjs";
+import { ActivatedRoute } from '@angular/router';
+import { RiskGroup } from "../models/risk-group";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormArray, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Location } from '@angular/common';
+import { Period } from "../models/period";
+import { PeriodType } from "../models/period-type";
+import { ConditionType } from "../models/condition-type";
+import { ClientAccount } from "../models/client-account";
+import { LookupDataService } from "../services/lookup-data.service";
+import { ConcessionCondition } from "../models/concession-condition";
 
 @Component({
-  selector: 'app-bol-add-concession',
-  templateUrl: './bol-add-concession.component.html',
-  styleUrls: ['./bol-add-concession.component.css']
+    selector: 'app-bol-add-concession',
+    templateUrl: './bol-add-concession.component.html',
+    styleUrls: ['./bol-add-concession.component.css']
 })
 export class BolAddConcessionComponent implements OnInit {
+    public bolConcessionForm: FormGroup;
+    private sub: any;
+    errorMessage: String;
+    validationError: String[];
+    saveMessage: String;
+    observableRiskGroup: Observable<RiskGroup>;
+    riskGroup: RiskGroup;
+    riskGroupNumber: number;
+    selectedConditionTypes: ConditionType[];
+    isLoading = false;
 
-    constructor(private location: Location) { }
+    constructor(private route: ActivatedRoute,
+        private formBuilder: FormBuilder,
+        private location: Location,
+        @Inject(LookupDataService) private lookupDataService) {
+        this.riskGroup = new RiskGroup();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.bolConcessionForm = this.formBuilder.group({
+            concessionItemRows: this.formBuilder.array([this.initConcessionItemRows()]),
+            conditionItemsRows: this.formBuilder.array([]),
+            mrsCrs: new FormControl(),
+            smtDealNumber: new FormControl(),
+            motivation: new FormControl(),
+            product: new FormControl()
+        });
+    }
 
-  goBack() {
-      this.location.back();
-  }
+    initConcessionItemRows() {
+        return this.formBuilder.group({
+            product: [''],
+            userId: [''],
+            unitRate: ['']
+        });
+    }
+
+    initConditionItemRows() {
+        this.selectedConditionTypes.push(new ConditionType());
+
+        return this.formBuilder.group({
+            conditionType: [''],
+            conditionProduct: [''],
+            interestRate: [''],
+            volume: [''],
+            value: [''],
+            periodType: [''],
+            period: ['']
+        });
+    }
+
+    goBack() {
+        this.location.back();
+    }
 
 }
