ea3bcdb24fadb4893a7d5b96a074aa15912cb786
Heathesh Bhandari <heath.bhandari@gmail.com>
2017-09-08 14:19:20 +0200

    Bug fixes: commented out conditions counts which are invalid and fixed bug when changing filter

--- code/src/StandardBank.ConcessionManagement.UI/client-src/app/conditions/conditions.component.ts
+++ code/src/StandardBank.ConcessionManagement.UI/client-src/app/conditions/conditions.component.ts
@@ -3,13 +3,13 @@ import { Subject } from 'rxjs/Rx';
 import { Condition } from '../models/condition';
 import { MyConditionService } from '../services/my-condition.service';
 import { Observable } from "rxjs";
-import { Period} from '../models/period';
+import { Period } from '../models/period';
 import { LookupDataService } from "../services/lookup-data.service";
 
 @Component({
-  selector: 'app-conditions',
-  templateUrl: './conditions.component.html',
-  styleUrls: ['./conditions.component.css']
+    selector: 'app-conditions',
+    templateUrl: './conditions.component.html',
+    styleUrls: ['./conditions.component.css']
 })
 export class ConditionsComponent implements OnInit {
     dtOptions: DataTables.Settings = {};
@@ -22,30 +22,31 @@ export class ConditionsComponent implements OnInit {
         @Inject(MyConditionService) private conditionService,
         @Inject(LookupDataService) private lookupDataService) { }
 
-  ngOnInit() {
-      this.dtOptions = {
-          pagingType: 'full_numbers',
-          language: {
-              emptyTable: "No records found!",
-              search: "",
-              searchPlaceholder: "Search"
-          }
-      };
-    
-      this.lookupDataService.getPeriods().subscribe(data => { this.periods = data; }, err => this.errorMessage = err);
-      this.GetConditions("3 Months", this.periodType);
+    ngOnInit() {
+        this.dtOptions = {
+            pagingType: 'full_numbers',
+            language: {
+                emptyTable: "No records found!",
+                search: "",
+                searchPlaceholder: "Search"
+            }
+        };
+
+        this.lookupDataService.getPeriods().subscribe(data => { this.periods = data; }, err => this.errorMessage = err);
+        this.GetConditions("3 Months", this.periodType);
     }
 
-  PeriodFilter(value:string) {
-      this.GetConditions(value, this.periodType);
-  }
+    PeriodFilter(value: string) {
+        this.dtTrigger = new Subject();
+        this.GetConditions(value, this.periodType);
+    }
 
-  GetConditions(period:string, periodType:string) {
-      this.conditionService.getMyConditions(period, periodType).subscribe(conditions => {
-          this.observableConditions = conditions;
-          this.dtTrigger.next();
-      },
-          error => this.errorMessage = <any>error);
-  }
+    GetConditions(period: string, periodType: string) {
+        this.conditionService.getMyConditions(period, periodType).subscribe(conditions => {
+            this.observableConditions = conditions;
+            this.dtTrigger.next();
+        },
+            error => this.errorMessage = <any>error);
+    }
 
 }
