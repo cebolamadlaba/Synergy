bf08deb6bb0cc208d7307a2e75583571bf8b7c7d
Sifiso <Sfiso.dlaba@gmail.com>
2017-09-13 14:52:45 +0200

    add script for running tests

---
+++ code/db/StandardBank.ConcessionManagement.Database.Migrator/Scripts/V1_2_4__32_addpricingfields.sql
@@ -0,0 +1,20 @@
+﻿ALTER TABLE dbo.tblConcessionCash ADD
+	fkApprovedTableNumberId int NULL
+GO
+ALTER TABLE dbo.tblConcessionCash ADD CONSTRAINT
+	FK_tblConcessionCash_rtblTableNumber_approved FOREIGN KEY
+	(fkApprovedTableNumberId) REFERENCES dbo.rtblTableNumber
+	(pkTableNumberId)
+GO
+ALTER TABLE [dbo].[tblConcessionCash] CHECK CONSTRAINT [FK_tblConcessionCash_rtblTableNumber_approved]
+GO
+ALTER TABLE dbo.tblConcessionTransactional ADD
+	fkApprovedTableNumberId int NULL
+GO
+ALTER TABLE dbo.tblConcessionTransactional ADD CONSTRAINT
+	FK_tblConcessionTransactional_rtblTableNumber_approved FOREIGN KEY
+	(fkApprovedTableNumberId) REFERENCES dbo.rtblTableNumber
+	(pkTableNumberId)
+GO
+ALTER TABLE [dbo].[tblConcessionTransactional] CHECK CONSTRAINT [FK_tblConcessionTransactional_rtblTableNumber_approved]
+GO
