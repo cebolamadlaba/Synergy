6af82ad5a22ac00128f79fb1a0ffa375bcfcbe92
Sifiso <Sfiso.dlaba@gmail.com>
2017-09-15 11:17:50 +0200

    add pricing columns. disable buttons on pricing page

--- code/db/StandardBank.ConcessionManagement.Database.Migrator/Scripts/V1_2_4__32_addpricingfields.sql
+++ code/db/StandardBank.ConcessionManagement.Database.Migrator/Scripts/V1_2_4__32_addpricingfields.sql
@@ -1,20 +1,39 @@
 ﻿ALTER TABLE dbo.tblConcessionCash ADD
-	fkApprovedTableNumberId int NULL
+	fkApprovedTableNumberId int NULL,
+	fkLoadedTableNumberId int NULL
 GO
 ALTER TABLE dbo.tblConcessionCash ADD CONSTRAINT
 	FK_tblConcessionCash_rtblTableNumber_approved FOREIGN KEY
 	(fkApprovedTableNumberId) REFERENCES dbo.rtblTableNumber
 	(pkTableNumberId)
 GO
+ALTER TABLE dbo.tblConcessionCash ADD CONSTRAINT
+	FK_tblConcessionCash_rtblTableNumber_loaded FOREIGN KEY
+	(fkLoadedTableNumberId) REFERENCES dbo.rtblTableNumber
+	(pkTableNumberId)
+GO
 ALTER TABLE [dbo].[tblConcessionCash] CHECK CONSTRAINT [FK_tblConcessionCash_rtblTableNumber_approved]
 GO
+ALTER TABLE [dbo].[tblConcessionCash] CHECK CONSTRAINT [FK_tblConcessionCash_rtblTableNumber_loaded]
+GO
 ALTER TABLE dbo.tblConcessionTransactional ADD
-	fkApprovedTableNumberId int NULL
+	fkApprovedTableNumberId int NULL,
+	fkLoadedTableNumberId int NULL
 GO
 ALTER TABLE dbo.tblConcessionTransactional ADD CONSTRAINT
 	FK_tblConcessionTransactional_rtblTableNumber_approved FOREIGN KEY
 	(fkApprovedTableNumberId) REFERENCES dbo.rtblTableNumber
 	(pkTableNumberId)
 GO
+ALTER TABLE dbo.tblConcessionTransactional ADD CONSTRAINT
+	FK_tblConcessionTransactional_rtblTableNumber_loaded FOREIGN KEY
+	(fkLoadedTableNumberId) REFERENCES dbo.rtblTableNumber
+	(pkTableNumberId)
+GO
+ALTER TABLE [dbo].[tblConcessionTransactional] CHECK CONSTRAINT [FK_tblConcessionTransactional_rtblTableNumber_loaded]
+GO
 ALTER TABLE [dbo].[tblConcessionTransactional] CHECK CONSTRAINT [FK_tblConcessionTransactional_rtblTableNumber_approved]
+GO
+ALTER TABLE dbo.tblConcessionLending ADD
+	LoadedMarginToPrime decimal(18,2) NULL
 GO
--- code/src/StandardBank.ConcessionManagement.UI/client-src/app/pricing-trade/pricing-trade.component.spec.ts
+++ code/src/StandardBank.ConcessionManagement.UI/client-src/app/pricing-trade/pricing-trade.component.spec.ts
@@ -1,4 +1,4 @@
-﻿import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpModule } from '@angular/http';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormArray, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { LookupDataService, MockLookupDataService } from "../services/lookup-data.service";
 import { PricingTradeComponent } from './pricing-trade.component';
+import { UserService, MockUserService } from '../services/user.service';
 
 describe('PricingTradeComponent', () => {
   let component: PricingTradeComponent;
@@ -15,7 +16,7 @@ describe('PricingTradeComponent', () => {
       TestBed.configureTestingModule({
           imports: [HttpModule, ModalModule.forRoot(), RouterTestingModule, FormsModule, ReactiveFormsModule],
           providers: [
-              { provide: LookupDataService, useClass: MockLookupDataService }
+              { provide: LookupDataService, useClass: MockLookupDataService }, { provide: UserService, useClass: MockUserService }
           ],
       declarations: [ PricingTradeComponent ]
     })
