54116d95f79da82fb5f26dfa25907299ebe30481
Heathesh Bhandari <heath.bhandari@gmail.com>
2017-11-08 11:35:11 +0200

    Bug fix: remove 'Suite Head' role

---
+++ code/db/StandardBank.ConcessionManagement.Database.Migrator/Scripts/V1_1_5__Bug_fix_changes.sql
@@ -0,0 +1,12 @@
+﻿DELETE FROM [dbo].[tblUserRole]
+WHERE [fkRoleId] IN (
+SELECT [pkRoleId] FROM [dbo].[rtblRole]
+WHERE [RoleName] in ('Suite Head ', 'Suite Head'))
+
+DELETE FROM [dbo].[rtblRole]
+WHERE [RoleName] = 'Suite Head '
+
+DELETE FROM [dbo].[rtblRole]
+WHERE [RoleName] = 'Suite Head'
+
+GO
