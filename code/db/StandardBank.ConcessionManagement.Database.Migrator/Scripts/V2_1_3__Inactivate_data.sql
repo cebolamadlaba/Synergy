3814fe1330685faec302e043e97201a126143d84
Heathesh Bhandari <heathesh@kohde.io>
2017-11-21 15:19:35 +0200

    Update: inactivate products, channels and transaction types that will not be used

---
+++ code/db/StandardBank.ConcessionManagement.Database.Migrator/Scripts/V2_1_3__Inactivate_data.sql
@@ -0,0 +1,29 @@
+﻿UPDATE [dbo].[rtblProduct]
+SET [IsActive] = 0
+WHERE [fkConcessionTypeId] in (
+SELECT [pkConcessionTypeId] FROM [dbo].[rtblConcessionType]
+WHERE [Code] = 'Lending')
+AND [Description] in ('Commercial Property Finance',
+'Debtors’ Finance',
+'VAF Instalment sale',
+'VAF Full maintenance lease',
+'VAF Operating Rental')
+
+GO
+
+UPDATE [dbo].[rtblChannelType]
+SET [IsActive] = 0
+WHERE [Description] = 'Bulk Teller'
+
+GO
+
+UPDATE [dbo].[rtblTransactionType]
+SET [IsActive] = 0
+WHERE [fkConcessionTypeId] IN (
+SELECT [pkConcessionTypeId] FROM [dbo].[rtblConcessionType]
+WHERE [Code] = 'Transactional')
+AND [Description] in ('Monthly Management Fee',
+'Automatic Cheque Clearance')
+
+GO
+
