b2c71788378dc24d608d6918ef01e3095faf12e0
BernardCronje <bernard@kohde.io>
2020-06-25 07:18:38 +0200

    Add sql script;

---
+++ code/db/StandardBank.ConcessionManagement.Database.Migrator/Scripts/V9_1_1_1_Addendum_1_8_2.sql
@@ -0,0 +1,9 @@
+﻿
+
+Create Table rtblExtensionFee
+(
+	pkExtensionFeeId Int Not Null Identity(1,1) Primary Key,
+	ExtensionFee Decimal(5,2) Not Null,
+	IsActive Bit Not Null,
+	DateEffectiveFrom DateTime Not Null
+)
