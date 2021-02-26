0c6c9460cf9ba9d58bd6302b86151ddce56c8c6d
cebo <cebo@kohde.io>
2021-02-26 12:46:28 +0200

    fix rounding of Fee and update button

--- code/src/StandardBank.ConcessionManagement.BusinessLogic/LetterGeneratorManager.cs
+++ code/src/StandardBank.ConcessionManagement.BusinessLogic/LetterGeneratorManager.cs
@@ -878,7 +878,7 @@ namespace StandardBank.ConcessionManagement.BusinessLogic
                     $" {lendingConcessionDetail.MarginAgainstPrime.ToString("N2", CultureInfo.InvariantCulture)}",
                 InitiationFee =
                     $" {lendingConcessionDetail.InitiationFee.ToString("N2", CultureInfo.InvariantCulture)}",
-                ReviewFee = $" {lendingConcessionDetail.ReviewFee.ToString("N2", CultureInfo.InvariantCulture)}",
+                ReviewFee = $" {lendingConcessionDetail.ReviewFee.ToString()}",
                 ConcessionEndDate = lendingConcessionDetail.ExpiryDate.Value.ToString("dd/MM/yyyy"),
                 ConcessionStartDate = lendingConcessionDetail.DateApproved.Value.ToString("dd/MM/yyyy"),
                 UFFFee = $" {lendingConcessionDetail.UffFee.ToString("N2", CultureInfo.InvariantCulture)}",
