9e0e454424cb2ad3082865d9632f4cdbc1ad30a9
janhdt <janhdt@kohde.io>
2021-03-04 11:44:59 +0200

    Add ATM Channel Type

---
+++ code/db/StandardBank.ConcessionManagement.Database/Change Scripts/ATM_Channel_Type/01_ATM_Channel_Type.sql
@@ -0,0 +1,9 @@
+
+
+
+insert into [rtblChannelType] (Description,IsActive,StandardPricingTable)
+values('ATM', 1 , 300)
+go
+insert into [rtblChannelTypeImport] (fkChannelTypeId,ImportFileChannel)
+values (10,122)
+
