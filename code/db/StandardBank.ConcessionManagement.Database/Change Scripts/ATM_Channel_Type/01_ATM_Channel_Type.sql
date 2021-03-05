444947db50c2860214398c60c9ee9a55222a83c6
janhdt <janhdt@kohde.io>
2021-03-05 08:13:19 +0200

    comment sugestions

--- code/db/StandardBank.ConcessionManagement.Database/Change Scripts/ATM_Channel_Type/01_ATM_Channel_Type.sql
+++ code/db/StandardBank.ConcessionManagement.Database/Change Scripts/ATM_Channel_Type/01_ATM_Channel_Type.sql
@@ -1,12 +1,17 @@
 
 declare @fkChannelTypeId int
 
-insert into [rtblChannelType] (Description,IsActive,StandardPricingTable)
-values('ATM', 1 , 300)
+if not exists(select pkChannelTypeId from [rtblChannelType] where Description = 'ATM')
+begin
+	insert into [rtblChannelType] (Description,IsActive,StandardPricingTable)
+	values('ATM', 1 , 300)
+end
 
- select @fkChannelTypeId = pkChannelTypeId from [rtblChannelType]
- where Description = 'ATM' --The description should alwais be unique 
+select @fkChannelTypeId = pkChannelTypeId from [rtblChannelType]
+where Description = 'ATM' --The description should alwais be unique 
 
-insert into [rtblChannelTypeImport] (fkChannelTypeId,ImportFileChannel)
-values (@fkChannelTypeId,122)
-
+if not exists(select pkChannelTypeImportId from [rtblChannelTypeImport] where fkChannelTypeId = @fkChannelTypeId)
+begin
+	insert into [rtblChannelTypeImport] (fkChannelTypeId,ImportFileChannel)
+	values (@fkChannelTypeId,122)
+end
